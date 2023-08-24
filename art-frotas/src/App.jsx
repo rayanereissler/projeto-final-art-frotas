e5de876df3decf55212de4f0a08870c80d26bc15
rayanereissler <reisslertec@gmail.com>
2023-08-24 19:46:45 -0300

    organizando as pastas do projeto

--- art-frotas/src/App.jsx
+++ art-frotas/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Nav/Navbar';
+import Carrousel from './Components/Carrousel/Carrousel';
+
 
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Carrousel/>
     </Router>
   )
 }
 
-export default App
+export default App;
---
+++ art-frotas/src/Components/Carrousel/Carrousel.jsx
@@ -0,0 +1,20 @@
+import { register } from 'swiper/element/bundle'
+register();
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+const Carrousel = () => {
+
+    return (
+        <div>
+            <h1>Carrousel</h1>
+        </div>
+    )
+}
+
+export default Carrousel;
+
