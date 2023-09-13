6f318ab154c3317a071df9c84efe1b9f46d13837
rayanereissler <reisslertec@gmail.com>
2023-09-12 22:40:30 -0300

    alterando o slide novamente

--- art-frotas/src/App.jsx
+++ art-frotas/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Navbar/>
-      {/* <Carrousel/> */}
+      <Carrousel/>
       <Footer/>
     </Router>
   
--- art-frotas/src/Components/Carrousel/Carrousel.jsx
+++ art-frotas/src/Components/Carrousel/Carrousel.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 
-import { Swiper, SwiperSlide } from 'swiper/react'; //importando os componentes do swiper react
-import 'swiper/css'; //importando o css do swiper
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
-import 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js';
 import './Carrousel.css';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import ChevroletOnix from '../../assets/imagens/Chevrolet-Onix.jpg';
 import HyundaiHb20 from '../../assets/imagens/hyundai-hb20.jpg';
@@ -15,182 +18,54 @@ import RenaultLogan from '../../assets/imagens/renault-logan.jpg';
 import VwGol from '../../assets/imagens/vw-gol.jpg';
 import VwUp from '../../assets/imagens/vw-up.jpg';
 
-export default () => {
+export default function Carrousel() {
     return (
-        <Swiper
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={4}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            {/* Slide 1 */}
-            <SwiperSlide id='container' className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={ChevroletOnix} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div className="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    Chevrolet Onix
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>4.5</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+        <>
+            <div className="container">
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 0,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 2.5,
+                    }}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="swiper_container"
+                >
+                    <SwiperSlide>
+                        <img src={ChevroletOnix} />
                     </SwiperSlide>
-
-                    {/* Slide 2 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={HyundaiHb20} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div className="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    Hyundai HB20
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>5.0</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={HyundaiHb20} />
                     </SwiperSlide>
-
-                    {/* Slide 3 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={JeepRenegade} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div class="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    Jeep Renegade
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>5.0</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={JeepRenegade} />
                     </SwiperSlide>
-
-                    {/* Slide 4 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={PicapeToro} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div className="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    Fiat Toro
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>5.0</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={PicapeToro} />
                     </SwiperSlide>
-
-                    {/* Slide 5 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={RenaultLogan} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div class="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    Renault Logan
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>4.5</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={RenaultLogan} />
                     </SwiperSlide>
-
-                    {/* Slide 6 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={VwGol} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div className="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    volkswagen Gol
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>4.5</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={VwGol} />
                     </SwiperSlide>
-
-                    {/* Slide 7 */}
-                    <SwiperSlide className="tranding-slide">
-                        <div className="tranding-slide-img">
-                            <img src={VwUp} alt="Tranding" />
-                        </div>
-                        <div className="tranding-slide-content">
-                            <div className="tranding-slide-content-bottom">
-                                <h2 className="food-name">
-                                    volkswagen UP
-                                </h2>
-                                <h3 className="food-rating">
-                                    <span>4.1</span>
-                                    <div className="rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                    </div>
-                                </h3>
-                            </div>
-                        </div>
+                    <SwiperSlide>
+                        <img src={VwUp} />
                     </SwiperSlide>
-        </Swiper>
+                    <SwiperSlide>
+                        <img src={PicapeToro} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={RenaultLogan} />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </>
     );
-};
+}
